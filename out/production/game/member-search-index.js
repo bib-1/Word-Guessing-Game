856ea8a2b6c836a6adbfc5a3ee689e51a8d785a1
Bibek Poudel <65732044+bib-1@users.noreply.github.com>
2021-11-21 16:24:34 -0500

    Deleted unnecessary javadoc files

--- out/production/game/member-search-index.js
+++
@@ -1,1 +0,0 @@
-memberSearchIndex = [{"p":"<Unnamed>","c":"Game","l":"Game()","u":"%3Cinit%3E()"},{"p":"<Unnamed>","c":"Game","l":"main(String[])","u":"main(java.lang.String[])"}];updateSearchResults();
